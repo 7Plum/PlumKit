b258c78199bde999b98ba13e1ae66e3558b38fb3
kingiol <kingxiaokang@gmail.com>
2015-12-06 21:46:37 +0800

    add crypto types & test playground

--- PlumKit/Foundation/NSData+PlumKit.swift
+++ PlumKit/Foundation/NSData+PlumKit.swift
@@ -7,29 +7,98 @@
 //
 
 import Foundation
+import CommonCrypto
+
+// MARK: - Crypto
 
 extension NSData {
+    
+    enum CryptoType: Int {
+        case MD2
+        case MD4
+        case MD5
+        case SHA1
+        case SHA224
+        case SHA256
+        case SHA384
+        case SHA512
+    }
 
-    func md5String() -> String {
-        return ""
-        //let str =
-        
-        
-//        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
-//        let strLen = CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-//        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-//        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
-//        
-//        CC_MD5(str!, strLen, result)
-//        
-//        var hash = NSMutableString()
-//        for i in 0..<digestLen {
-//            hash.appendFormat("%02x", result[i])
-//        }
-//        
-//        result.dealloc(digestLen)
-//        
-//        return String(format: hash)
+    public var MD2: NSData {
+        let hash = digestHash(.MD2)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var MD4: NSData {
+        let hash = digestHash(.MD4)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var MD5: NSData {
+        let hash = digestHash(.MD5)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var SHA1: NSData {
+        let hash = digestHash(.SHA1)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var SHA224: NSData {
+        let hash = digestHash(.SHA224)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var SHA256: NSData {
+        let hash = digestHash(.SHA256)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var SHA384: NSData {
+        let hash = digestHash(.SHA384)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    public var SHA512: NSData {
+        let hash = digestHash(.SHA512)
+        return NSData(bytes: hash, length: hash.count)
+    }
+    
+    private func digestHash(cryptoType: CryptoType) -> [UInt8] {
+        switch cryptoType {
+        case .MD2:
+            var hash = [UInt8](count: Int(CC_MD2_DIGEST_LENGTH), repeatedValue: 0)
+            CC_MD2(bytes, CC_LONG(length), &hash)
+            return hash
+        case .MD4:
+            var hash = [UInt8](count: Int(CC_MD4_DIGEST_LENGTH), repeatedValue: 0)
+            CC_MD4(bytes, CC_LONG(length), &hash)
+            return hash
+        case .MD5:
+            var hash = [UInt8](count: Int(CC_MD5_DIGEST_LENGTH), repeatedValue: 0)
+            CC_MD5(bytes, CC_LONG(length), &hash)
+            return hash
+        case .SHA1:
+            var hash = [UInt8](count: Int(CC_SHA1_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA1(bytes, CC_LONG(length), &hash)
+            return hash
+        case .SHA224:
+            var hash = [UInt8](count: Int(CC_SHA224_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA224(bytes, CC_LONG(length), &hash)
+            return hash
+        case .SHA256:
+            var hash = [UInt8](count: Int(CC_SHA256_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA256(bytes, CC_LONG(length), &hash)
+            return hash
+        case .SHA384:
+            var hash = [UInt8](count: Int(CC_SHA384_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA384(bytes, CC_LONG(length), &hash)
+            return hash
+        case .SHA512:
+            var hash = [UInt8](count: Int(CC_SHA512_DIGEST_LENGTH), repeatedValue: 0)
+            CC_SHA512(bytes, CC_LONG(length), &hash)
+            return hash
+        }
     }
     
 }
--- PlumKit/Foundation/String+PlumKit.swift
+++ PlumKit/Foundation/String+PlumKit.swift
@@ -15,31 +15,80 @@
 import Foundation
 import CommonCrypto
 
+// MARK: - Crypto
 
 public extension String {
     
     /**
-     Returns a lowercase String for md5 hash.
+     Return a String for MD2 hash.
     */
-    public func md5String() -> String {
-        return ""
-        
-        
-        //        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
-        //        let strLen = CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-        //        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        //        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
-        //
-        //        CC_MD5(str!, strLen, result)
-        //
-        //        var hash = NSMutableString()
-        //        for i in 0..<digestLen {
-        //            hash.appendFormat("%02x", result[i])
-        //        }
-        //
-        //        result.dealloc(digestLen)
-        //        
-        //        return String(format: hash)
+    public var MD2: String? {
+        return String(hashData: hashData?.MD2, length: CC_MD2_DIGEST_LENGTH)
     }
     
+    /**
+     Return a String for MD4 hash.
+    */
+    public var MD4: String? {
+        return String(hashData: hashData?.MD4, length: CC_MD4_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for MD5 hash.
+    */
+    public var MD5: String? {
+        return String(hashData: hashData?.MD5, length: CC_MD5_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for SHA1 hash.
+     */
+    public var SHA1: String? {
+        return String(hashData: hashData?.SHA1, length: CC_SHA1_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for SHA224 hash.
+     */
+    public var SHA224: String? {
+        return String(hashData: hashData?.SHA224, length: CC_SHA224_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for SHA256 hash.
+    */
+    public var SHA256: String? {
+        return String(hashData: hashData?.SHA256, length: CC_SHA256_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for SHA384 hash.
+     */
+    public var SHA384: String? {
+        return String(hashData: hashData?.SHA384, length: CC_SHA384_DIGEST_LENGTH)
+    }
+    
+    /**
+     Return a String for SHA512 hash.
+     */
+    public var SHA512: String? {
+        return String(hashData: hashData?.SHA512, length: CC_SHA512_DIGEST_LENGTH)
+    }
+    
+    private var hashData: NSData? {
+        return dataUsingEncoding(NSUTF8StringEncoding)
+    }
+    
+    private init?(hashData: NSData?, length: Int32) {
+        guard let hashData = hashData else { return nil }
+        
+        var digest = [UInt8](count: Int(length), repeatedValue: 0)
+        hashData.getBytes(&digest, length: Int(length) * sizeof(UInt8))
+        
+        var string = ""
+        for i in 0..<length {
+            string += String(format: "%02x", digest[Int(i)])
+        }
+        self.init(string)
+    }
 }
