0cd83ef9cc37d812b424d3e2ec2bbfffc149fe78
kingiol <kingxiaokang@gmail.com>
2015-12-06 14:07:02 +0800

    init

---
+++ PlumKit/PlumKit.h
@@ -0,0 +1,19 @@
+//
+//  PlumKit.h
+//  PlumKit
+//
+//  Created by Kingiol on 15/12/6.
+//  Copyright © 2015年 Kingiol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlumKit.
+FOUNDATION_EXPORT double PlumKitVersionNumber;
+
+//! Project version string for PlumKit.
+FOUNDATION_EXPORT const unsigned char PlumKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlumKit/PublicHeader.h>
+
+
